2cc3b835efa82169f83fd4aee2c85aa9dcf4f4a0
Happy Kumar <happy020394@gmail.com>
2023-12-26 00:23:46 +0530

    edit client.js8

--- public/client.js
+++ public/client.js
@@ -8,25 +8,7 @@ var dashboard = document.querySelector("#dashboard"),
 
 const iceServers = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "stun:stun2.l.google.com:19302" },
-    { urls: "stun:stun3.l.google.com:19302" },
-    { urls: "stun:stun4.l.google.com:19302" },
-    { urls: "stun:stun01.sipphone.com" },
-    { urls: "stun:stun.ekiga.net" },
-    { urls: "stun:stun.fwdnet.net" },
-    { urls: "stun:stun.ideasip.com" },
-    { urls: "stun:stun.iptel.org" },
-    { urls: "stun:stun.rixtelecom.se" },
-    { urls: "stun:stun.schlund.de" },
-    { urls: "stun:stunserver.org" },
-    { urls: "stun:stun.softjoys.com" },
-    { urls: "stun:stun.voiparound.com" },
-    { urls: "stun:stun.voipbuster.com" },
-    { urls: "stun:stun.voipstunt.com" },
-    { urls: "stun:stun.voxgratia.org" },
-    { urls: "stun:stun.xten.com" }
+    { urls: "stun:stun.l.google.com:19302" }
   ]
 };
 
@@ -103,8 +85,6 @@ socket.on("receive-streaming", () => {
       .catch(err => {
         console.error("Error creating or setting local description:", err);
       });
-  } else {
-    console.warn("Invalid signaling state for offer:", pc.signalingState);
   }
 });
 
