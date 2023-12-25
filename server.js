e1118669c36158abafe14d3fa7e8e17f9a8b2f04
Happy Kumar <happy020394@gmail.com>
2023-12-25 18:25:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const PORT = process.env.PORT || 3000;
+
+// paste your IP address of the local network
+const HOST = "192.168.29.192";
+
+// specify the self-signed certificate for https connection
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem")
+};
+const server = require("https").createServer(options, app);
+const io = require("socket.io")(server);
+
+app.use(express.static("public"));
+
+// Default route for Heroku
+app.get("/", (req, res) => {
+  res.send("WebRTC application running on Heroku!");
+});
+
+io.on("connection", socket => {
+  console.log("user connected to the socket");
+
+  socket.on("start-streaming", () => {
+    // Broadcast that the source PC is starting streaming
+    io.emit("start-streaming");
+  });
+
+  socket.on("receive-streaming", () => {
+    // Broadcast that the second PC is ready to receive streaming
+    io.emit("receive-streaming");
+  });
+
+  socket.on("offer", offer => {
+    // Broadcast the offer to all connected clients
+    io.emit("offer", offer);
+  });
+
+  socket.on("answer", answer => {
+    // Broadcast the answer to all connected clients
+    io.emit("answer", answer);
+  });
+
+  socket.on("candidate", candidate => {
+    // Broadcast the candidate to all connected clients
+    io.emit("candidate", candidate);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server is listening on port:", PORT);
+});
