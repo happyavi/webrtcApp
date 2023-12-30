fb6dd52bd3ab1e3f5637db45ad27e4555930665d
Happy Kumar <happy020394@gmail.com>
2023-12-30 12:57:43 +0530

    editfina

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path");
-const multer = require("multer");
+const fileUpload = require("express-fileupload");
+
+app.use(fileUpload());
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,24 +13,46 @@ const io = require("socket.io")(server);
 
 app.use(express.static("public"));
 
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "public"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
+app.get("/", (req, res) => {
+  res.send("WebRTC application running on Heroku!");
 });
 
-const upload = multer({ storage: storage });
+app.post("/upload", (req, res) => {
+  const file = req.files.file;
+  const fileName = file.name;
+  const filePath = path.join(__dirname, "public", fileName);
 
-app.post("/upload", upload.single("file"), (req, res) => {
-  res.json({ fileName: req.file.filename });
+  file.mv(filePath, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName });
+  });
 });
 
 io.on("connection", (socket) => {
-  // ... (rest of the code remains unchanged)
+  console.log("user connected to the socket");
+
+  socket.on("start-streaming", () => {
+    io.emit("start-streaming");
+  });
+
+  socket.on("receive-streaming", () => {
+    io.emit("receive-streaming");
+  });
+
+  socket.on("offer", (offer) => {
+    io.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    io.emit("answer", answer);
+  });
+
+  socket.on("candidate", (candidate) => {
+    io.emit("candidate", candidate);
+  });
 });
 
 server.listen(PORT, () => {
