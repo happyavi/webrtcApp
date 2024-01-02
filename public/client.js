34efbb7bcb655deaed0197e1576d5290851cdf64
Happy Kumar <happy020394@gmail.com>
2024-01-02 22:44:31 +0530

    edits

--- public/client.js
+++ public/client.js
@@ -1,71 +1,99 @@
 var dashboard = document.querySelector("#dashboard"),
-  stream = document.querySelector("#stream"),
-  client = document.querySelector("#client"),
-  connect = document.querySelector("#connect"),
-  guest = document.querySelector("#guest"),
-  hangUp = document.querySelector("#hang-up");
+  stream = document.querySelector("#stream"),
+  client = document.querySelector("#client"),
+  connect = document.querySelector("#connect"),
+  guest = document.querySelector("#guest"),
+  hangUp = document.querySelector("#hang-up");
 
-const iceServers = {
-  iceServers: [
-    { urls: "stun:stun.l.google.com:19302" }
-  ]
-};
+const stunServers = [
+  "stun.l.google.com:19302",
+  "stun1.l.google.com:19302",
+  "stun2.l.google.com:19302",
+  "stun3.l.google.com:19302",
+  "stun4.l.google.com:19302",
+  "stun01.sipphone.com",
+  "stun.ekiga.net",
+  "stun.fwdnet.net",
+  "stun.ideasip.com",
+  "stun.iptel.org",
+  "stun.rixtelecom.se",
+  "stun.schlund.de",
+  "stunserver.org",
+  "stun.softjoys.com",
+  "stun.voiparound.com",
+  "stun.voipbuster.com",
+  "stun.voipstunt.com",
+  "stun.voxgratia.org",
+  "stun.xten.com"
+];
 
-const pc = new RTCPeerConnection(iceServers);
+const pc = new RTCPeerConnection();
 const socket = io("https://webrtcappm-cf49c223a6aa.herokuapp.com");
 
 var localeStream;
 var isSource = false;
 
 // Log RTCPeerConnection state changes
-pc.addEventListener('iceconnectionstatechange', () => {
-  console.log('ICE connection state:', pc.iceConnectionState);
+pc.addEventListener("iceconnectionstatechange", () => {
+  console.log("ICE connection state:", pc.iceConnectionState);
 });
 
-pc.addEventListener('signalingstatechange', () => {
-  console.log('Signaling state:', pc.signalingState);
+pc.addEventListener("signalingstatechange", () => {
+  console.log("Signaling state:", pc.signalingState);
 });
 
-pc.addEventListener('connectionstatechange', () => {
-  console.log('Connection state:', pc.connectionState);
+pc.addEventListener("connectionstatechange", () => {
+  console.log("Connection state:", pc.connectionState);
 });
 
 hangUp.onclick = function (e) {
-  location.reload();
+  location.reload();
 };
 
 connect.onclick = function () {
-  // Trigger "start-streaming" event when the "Start Streaming" button is clicked
-  socket.emit("start-streaming");
-  dashboard.style.display = "none";
-  stream.style.display = "block";
-  isSource = true; // Set the user as the source
+  // Trigger "start-streaming" event when the "Start Streaming" button is clicked
+  socket.emit("start-streaming");
+  dashboard.style.display = "none";
+  stream.style.display = "block";
+  isSource = true; // Set the user as the source
 };
 
 guest.onclick = function () {
-  // Trigger "receive-streaming" event when the "Receive Streaming" button is clicked
-  socket.emit("receive-streaming");
-  dashboard.style.display = "none";
-  stream.style.display = "block";
+  // Trigger "receive-streaming" event when the "Receive Streaming" button is clicked
+  socket.emit("receive-streaming");
+  dashboard.style.display = "none";
+  stream.style.display = "block";
 };
 
+function initializeConnectionWithStunServers(index) {
+  if (index >= stunServers.length) {
+    console.error("Unable to connect to any STUN server.");
+    return;
+  }
+
+  const currentStunServer = stunServers[index];
+
+  const iceServers = {
+    iceServers: [{ urls: `stun:${currentStunServer}` }]
+  };
+
+  pc.setConfiguration({ iceServers });
+
 socket.on("start-streaming", () => {
-  // get user media
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: true })
-    .then(async userStream => {
-      if (isSource) {
-        // If the user is the source, display their own stream
-        client.srcObject = userStream;
-      }
-      localeStream = userStream;
-      try {
-        client.play();
-      } catch (err) {
-        console.error(err);
-      }
-    });
-});
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then(async (userStream) => {
+        if (isSource) {
+          client.srcObject = userStream;
+        }
+        localeStream = userStream;
+        try {
+          client.play();
+        } catch (err) {
+          console.error(err);
+        }
+      });
+  });
 
 socket.on("receive-streaming", () => {
   // Set up the PC for receiving streaming
