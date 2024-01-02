ebe90021f142f6e6c038b1b517b12126371a8dcc
Happy Kumar <happy020394@gmail.com>
2024-01-03 03:03:57 +0530

    changee2

--- public/client.js
+++ public/client.js
@@ -9,7 +9,9 @@ const iceServers = {
   iceServers: [
     //{ urls: "stun:stun.l.google.com:19302" },
     {
-      urls: "turn:turn01.hubl.in?transport=udp"
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      username: "28224511:1379330808",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA="
     }
   ]
 };
